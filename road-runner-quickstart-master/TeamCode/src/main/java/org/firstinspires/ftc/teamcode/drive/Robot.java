bea0bcfb7d0b6ca6f4e1853dc7c43519c94bfedb
Albert24GG <guiman.albert09@gmail.com>
2023-02-11 14:16:12 +0200

    Add LinearSlide States

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Robot.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Robot.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.drive;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -58,6 +61,132 @@ public class Robot {
             }
         }
     }
+
+    public static class LinearSlide {
+        public enum State {
+            SLIDER_GROUND(initialPosition),
+            SLIDER_LOW(750),
+            SLIDER_MIDDLE(1500),
+            SLIDER_HIGH(2200);
+
+            private int position;
+
+            private State(int position) {
+                this.position = position;
+            }
+
+            public int getPosition() {
+                return position;
+            }
+        }
+
+        public enum Stage {
+            SLIDER_MOVING_UP,
+            SLIDER_MOVING_DOWN,
+            SLIDER_STATIONARY
+        }
+
+        public static DcMotorEx motorSlideLeft = null;
+        public static DcMotorEx motorSlideRight = null;
+        public static int initialPosition = 0;
+
+        public State state = null;
+        public Stage stage = null;
+
+        public void init(HardwareMap hardwareMap) {
+            motorSlideLeft = hardwareMap.get(DcMotorEx.class, "motorSlideLeft");
+            motorSlideRight = hardwareMap.get(DcMotorEx.class, "motorSlideRight");
+
+            motorSlideLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorSlideRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            initialPosition = motorSlideLeft.getCurrentPosition();
+
+            motorSlideRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            motorSlideRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorSlideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorSlideRight.setPower(0.5);
+            motorSlideLeft.setPower(0.5);
+            motorSlideLeft.setTargetPosition(motorSlideLeft.getCurrentPosition());
+            motorSlideRight.setTargetPosition(motorSlideRight.getCurrentPosition());
+
+            state = State.SLIDER_GROUND;
+            stage = Stage.SLIDER_STATIONARY;
+
+        }
+
+        public void setTargetPosition(int position) {
+            motorSlideLeft.setTargetPosition(position);
+            motorSlideRight.setTargetPosition(position);
+        }
+
+        private void setMotorsPower(double power) {
+            motorSlideLeft.setPower(power);
+            motorSlideRight.setPower(power);
+        }
+
+        private boolean motorsAtTarget() {
+            return (motorSlideRight.getCurrentPosition() == motorSlideRight.getTargetPosition() && motorSlideLeft.getCurrentPosition() == motorSlideLeft.getTargetPosition());
+        }
+
+        private void motorsOn() {
+            setMotorsPower(0.5);
+        }
+
+        private void motorsOff() {
+            setMotorsPower(0);
+        }
+
+        private boolean slidersMoving() {
+            return (stage == Stage.SLIDER_MOVING_UP || stage == Stage.SLIDER_MOVING_DOWN);
+        }
+
+        public void moveUp() {
+            switch (state) {
+                case SLIDER_GROUND:
+                    state = State.SLIDER_LOW;
+                    break;
+
+                case SLIDER_LOW:
+                    state = State.SLIDER_MIDDLE;
+                    break;
+
+                case SLIDER_MIDDLE:
+                    state = State.SLIDER_HIGH;
+                    break;
+            }
+            stage = Stage.SLIDER_MOVING_UP;
+            setTargetPosition(state.getPosition());
+            motorsOn();
+        }
+
+        public void moveDown() {
+            switch (state) {
+                case SLIDER_HIGH:
+                    state = State.SLIDER_MIDDLE;
+                    break;
+
+                case SLIDER_MIDDLE:
+                    state = State.SLIDER_LOW;
+                    break;
+
+                case SLIDER_LOW:
+                    state = State.SLIDER_GROUND;
+            }
+            stage = Stage.SLIDER_MOVING_DOWN;
+            setTargetPosition(state.getPosition());
+            motorsOn();
+        }
+
+        public void update() {
+            if (slidersMoving() && motorsAtTarget()) {
+                stage = Stage.SLIDER_STATIONARY;
+                if(state == State.SLIDER_GROUND) motorsOff();
+            }
+        }
+
+    }
+
     public static class Claw {
         public enum State {
             CLAW_OPEN,
@@ -69,7 +198,9 @@ public class Robot {
             CLAW_DOWN,
             CLAW_ROTATING_UP,
             CLAW_ROTATING_DOWN,
-        };
+        }
+
+        ;
 
         static final double SERVO_OPEN = 0; //de updatat!!!
         static final double SERVO_CLOSED = 0.8; //de updatat!!!
